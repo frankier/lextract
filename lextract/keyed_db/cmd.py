e1c402a187a1e65183cc2afb3afc1f35d675a4f9
Frankie Robertson <frankie@robertson.name>
2019-12-16 13:29:00 +0200

    Add repl for extract_toks

--- lextract/keyed_db/cmd.py
+++ lextract/keyed_db/cmd.py
@@ -3,11 +3,17 @@ from wikiparse.cmd.mk_db import mk_cmds
 
 from lextract.keyed_db.tables import metadata
 from lextract.keyed_db.builddb import add_keyed_words
+from lextract.keyed_db.repl import extract_toks_cmd
 
 
 db_group = mk_cmds(metadata)
 
 merged = click.CommandCollection(
-    sources=[db_group, click.Group(commands={"add_keyed_words": add_keyed_words})],
+    sources=[db_group, click.Group(
+        commands={
+            "add_keyed_words": add_keyed_words,
+            "extract-toks": extract_toks_cmd
+        }
+    )],
     help="Commands for lextract ETL",
 )
---
+++ lextract/keyed_db/repl.py
@@ -0,0 +1,23 @@
+import sys
+import click
+from pprint import pprint
+from finntk import get_omorfi
+from wikiparse.utils.db import get_session
+
+from .extract import extract_toks
+
+
+@click.command("extract-toks")
+def extract_toks_cmd():
+    paragraph = sys.stdin.read()
+    omorfi = get_omorfi()
+    tokenised = omorfi.tokenise(paragraph)
+    starts = []
+    start = 0
+    for token in tokenised:
+        start = paragraph.index(token['surf'], start)
+        starts.append(start)
+
+    surfs = [tok["surf"] for tok in tokenised]
+    session = get_session().get_bind()
+    pprint(list(extract_toks(session, surfs)))
