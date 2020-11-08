7a29a745f4a42e571d98deed9ab44b2bb0630f2d
Frankie Robertson <frankie@robertson.name>
2020-11-08 11:59:05 +0200

    Add json data required by tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,11 @@
 import os
 from lextract.keyed_db.tables import extend_mweproc
+from wikiparse.utils.mod_data import set_jsons_path
 from wikiparse.utils.fst import LazyFst
 
 
 def pytest_configure(config):
+    set_jsons_path(os.path.join(os.path.dirname(__file__), "data/mod_data"))
     if "FST_DIR" in os.environ:
         LazyFst.set_fst_dir(os.environ["FST_DIR"])
     extend_mweproc()
